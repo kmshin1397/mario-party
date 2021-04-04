7d3451092351a83daf483ec60e8b34145ce650dd
kshin <kshin@caltech.edu>
2021-04-04 16:45:41 -0400

    Update README

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
     databaseURL: "https://mario-party-a4e41.firebaseio.com",
     projectId: "mario-party-a4e41",
     storageBucket: "mario-party-a4e41.appspot.com",
-    messagingSenderId: ""
-  }
+    messagingSenderId: "",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
     databaseURL: "https://mario-party-a4e41.firebaseio.com",
     projectId: "mario-party-a4e41",
     storageBucket: "mario-party-a4e41.appspot.com",
-    messagingSenderId: ""
-  }
+    messagingSenderId: "",
+  },
 };
 
 /*
